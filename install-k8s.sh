90a04d6c6dbcc0df077babb5ba3569208ae0137f
sieginglion <sieginglion@gmail.com>
2023-12-27 20:37:51 +0800

    install k8s

---
+++ install-k8s.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cat <<EOF >/etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+modprobe overlay
+modprobe br_netfilter
+cat <<EOF >/etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward = 1
+EOF
+sysctl -p /etc/sysctl.d/k8s.conf
+
+apt-get update
+apt-get install --no-install-recommends -y containerd
+mkdir /etc/containerd
+containerd config default | sed 's/SystemdCgroup = false/SystemdCgroup = true/' >/etc/containerd/config.toml
+systemctl restart containerd
+
+curl -L https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/k8s.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/k8s.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' >/etc/apt/sources.list.d/k8s.list
+apt-get update
+apt-get install --no-install-recommends -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
+export KUBECONFIG=/etc/kubernetes/admin.conf
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+
+# Install K9s
+curl -LO https://github.com/derailed/k9s/releases/download/v0.30.4/k9s_Linux_amd64.tar.gz
+tar -xf k9s_Linux_amd64.tar.gz -C /usr/local/bin
